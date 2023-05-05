6fe50285ab47a08273152a631cecbcc0c5092e42
Ekaterina Raptunovich <katj0410@gmail.com>
2023-05-05 14:58:00 +0300

    Added deletion of images in gallery and product

--- routes/admin_products.js
+++ routes/admin_products.js
@@ -242,13 +242,74 @@ router.post('/edit-product/:id', function(req, res){
 
 });
 
-//get delete page
+//post product gallery
+
+router.post('/product-gallery/:id', function (req, res) {
+    var productImage = req.files.file;
+	var id = req.params.id;
+	var path = "public/product_images/"+id+"/gallery/"+req.files.file.name;
+	var thumbsPath = "public/product_images/"+id+"/gallery/thumbs/"+req.files.file.name;
+
+	console.log("Path: "+path);
+	productImage.mv(path, function(err){
+		if(err) console.log(err);
+
+		resizeImg(fs.readFileSync(path), {width: 100, height: 100}).then(function(buf){
+			fs.writeFileSync(thumbsPath, buf);
+	});
+	});
+
+	res.sendStatus(200);
+});
+
+//get delete image
+
+router.get('/delete-image/:image', function (req, res) {
+	var originalImage = "public/product_images/"+req.query.id+"/gallery/"+req.params.image;
+	var thumbImage = "public/product_images/"+req.query.id+"/gallery/thumbs/"+req.params.image;
+
+	fs.remove(originalImage, function(err){
+		if(err){
+			console.log(err);
+		} else{
+			fs.remove(thumbImage, function(err){
+				if(err){
+					console.log(err);
+				} else{
+					req.flash('success', 'Изображение удалено!!');
+					res.redirect('/admin/products/edit-product/'+req.query.id);
+				}
+			});
+		}
+	})
+});
+
+//get delete category
 
 router.get('/delete-category/:id', function (req, res) {
     Page.findByIdAndRemove(req.params.id, function(err){
 		if(err) console.log(err);
 		req.flash('success', 'Категория была успешно удалена!');
 		res.redirect('/admin/categories/');
+	});
+});
+
+//get delete product
+
+router.get('/delete-product/:id', function (req, res) {
+    var id = req.params.id;
+	var path = "public/product_images/"+id;
+
+	fs.remove(path, function(err){
+		if(err){
+			console.log(err);
+		} else {
+			Product.findByIdAndRemove(id, function(err){
+				console.log(err);
+			});
+			req.flash('success', 'Товар был успешно удален!');
+			res.redirect('/admin/products');
+		}
 	});
 });
 
